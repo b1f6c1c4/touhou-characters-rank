e4feb5edcc9af8db5f435c818a8067a98c3a6abc
b1f6c1c4 <b1f6c1c4@gmail.com>
2024-05-15 01:32:32 -0400

    tooltip

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const tmpl = document.getElementById('tooltip-tmpl').innerHTML;
+const closer = document.getElementById('close');
 function getContrastColor(c) {
   const color = (c.charAt(0) === '#') ? c.substring(1, 7) : c;
   const R = parseInt(color.substring(0, 2), 16);
@@ -14,6 +15,9 @@ fetch('/touhou.json').then(async (res) => {
   series.forEach(s => {
     s.borderWidth = 2;
     s.showLine = true;
+    s.order = 1;
+    s.tension = 0.3;
+    s.pointRadius = 3;
   });
   const options = {
     type: 'scatter',
@@ -21,6 +25,7 @@ fetch('/touhou.json').then(async (res) => {
       datasets: series,
     },
     options: {
+      pointHitRadius: 9,
       animation: false,
       maintainAspectRatio: false,
       scales: {
@@ -36,6 +41,15 @@ fetch('/touhou.json').then(async (res) => {
       },
       events: ['click'],
       plugins: {
+        legend: {
+          labels: {
+            font: {
+              size: 8,
+            },
+            padding: 3,
+            boxWidth: 7,
+          },
+        },
         tooltip: {
           enabled: false,
           external(context) {
@@ -45,7 +59,8 @@ fetch('/touhou.json').then(async (res) => {
             // Hide if no tooltip
             const tooltipModel = context.tooltip;
             if (tooltipModel.opacity === 0) {
-              tooltipEl.style.opacity = 0;
+              tooltipEl.style.display = 'none';
+              closer.style.visibility = 'hidden';
               return;
             }
 
@@ -58,54 +73,96 @@ fetch('/touhou.json').then(async (res) => {
             }
 
             // Set Text
+            let ids = new Map();
             if (tooltipModel.body) {
-              const titleLines = tooltipModel.title || [];
-              const bodyLines = tooltipModel.body.map(bi => bi.lines);
-              let o;
-              tooltipEl.innerHTML = Mustache.render(tmpl, o = {
+              tooltipModel.body.forEach((bi, i) => ids.set(tooltipModel.dataPoints[i].datasetIndex, i));
+              tooltipEl.innerHTML = Mustache.render(tmpl, {
                 title: tooltipModel.title || [],
-                body: tooltipModel.body.map((bi, i) => {
+                body: Array.from(ids, ([id, i]) => {
                   const colors = tooltipModel.labelColors[i];
                   const style = `
                     background: ${colors.backgroundColor};
                     border-color: ${colors.borderColor};
                     color: ${getContrastColor(colors.backgroundColor)};
                   `;
-                  const { label, aux } = series[tooltipModel.dataPoints[i].datasetIndex];
+                  const { label, aux } = series[id];
+                  const m = label.match(/^(?<l>[^（）]*)(?<l2>（.*）)?$/);
+                  const { l, l2 } = m.groups;
                   return {
                     style,
-                    label,
-                    cls: label.length <= 4 ? 'nm-lg' : label.length <= 6 ? 'nm-md' : 'nm-sm',
+                    cls: l.length <= 4 ? 'nm-lg' : l.length <= 6 ? 'nm-md' : 'nm-sm',
+                    label: l,
+                    label2: l2,
                     ...aux,
+                    dt: new Date(aux.dt).toISOString().replace(/T.*/, ''),
                     v: Math.round(Math.log(aux.v) * 1000) / 1000,
                   };
                 }),
               });
-              console.dir(o);
             }
 
             const position = context.chart.canvas.getBoundingClientRect();
             const bodyFont = Chart.helpers.toFont(tooltipModel.options.bodyFont);
 
             // Display, position, and set styles for font
+            closer.style.visibility = 'visible';
             tooltipEl.style.opacity = 1;
+            tooltipEl.style.display = 'flex';
             tooltipEl.style.position = 'absolute';
-            tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX + 'px';
-            tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY + 'px';
+            if (ids.size === 1) {
+              if (tooltipModel.caretX > position.width / 2) {
+                tooltipEl.style.left = 'unset';
+                tooltipEl.style.right = window.innerWidth - position.right - window.scrollX + position.width - tooltipModel.caretX + 'px';
+              } else {
+                tooltipEl.style.left = position.left + window.scrollX + tooltipModel.caretX + 'px';
+                tooltipEl.style.right = 'unset';
+              }
+              if (tooltipModel.caretY > position.height / 2) {
+                tooltipEl.style.top = 'unset';
+                tooltipEl.style.bottom = window.innerHeight - position.bottom - window.scrollY + position.height - tooltipModel.caretY + 'px';
+              } else {
+                tooltipEl.style.top = position.top + window.scrollY + tooltipModel.caretY + 'px';
+                tooltipEl.style.bottom = 'unset';
+              }
+              tooltipEl.style.width = 'unset';
+            } else {
+              tooltipEl.style.left = '0';
+              tooltipEl.style.right = 'unset';
+              tooltipEl.style.width = '100vw';
+              tooltipEl.style.top = '0';
+              tooltipEl.style.bottom = 'unset';
+            }
             tooltipEl.style.font = bodyFont.string;
             tooltipEl.style.padding = tooltipModel.padding + 'px ' + tooltipModel.padding + 'px';
-            tooltipEl.style.pointerEvents = 'none';
           },
         },
       },
       onHover: (e, as, chart) => {
-        if (as.length) {
-          const [a] = as;
-          console.dir(a);
+        series.forEach(s => {
+          s.order = 1;
+          s.borderWidth = as.length ? 0.1 : 2;
+          s.pointRadius = 3;
+        });
+        as.forEach(a => {
           const obj = series[a.datasetIndex];
-        }
+          obj.order = 0;
+          obj.borderWidth = 5;
+          obj.pointRadius = 8;
+        });
+        chart.update();
       },
     },
   };
-  new Chart(document.getElementById('chart'), options);
+  const theChart = new Chart(document.getElementById('chart'), options);
+  closer.addEventListener('click', () => {
+    theChart.setActiveElements([]);
+    series.forEach(s => {
+      s.order = 1;
+      s.borderWidth = 2;
+      s.pointRadius = 3;
+    });
+    tooltipEl.style.display = 'none';
+    closer.style.visibility = 'hidden';
+    theChart.update();
+  });
 });
